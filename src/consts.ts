fb8cd0b66263621f7e4580bb24394aca638388aa
jaburks79 <jaburks79@gmail.com>
2026-06-14 17:05:54 -0500

    Initial YoungishNomads site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Youngish Nomads';
+export const SITE_DESCRIPTION = 'Two empty nesters traveling the world and staying youngish at heart. Real trips, honest takes, and life after kids.';
+
